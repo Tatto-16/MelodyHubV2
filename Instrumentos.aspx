42104479217120682442b3993ed53556c0ea049b
Santiago <eriksanrg1206@gmail.com>
2024-12-06 21:24:29 -0500

    Add files via upload

---
+++ Instrumentos.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Instrumentos.aspx.cs" Inherits="MelodyHub.instrumentos.Instrumentos" %>
+<asp:Content ID="head" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="title" ContentPlaceHolderID="title" runat="server">
+</asp:Content>
+<asp:Content ID="gvInstrumento" ContentPlaceHolderID="main" runat="server">
+    <h2>Gestión de Instrumentos</h2>
+    <asp:GridView ID="gvInstrumentos" runat="server" AutoGenerateColumns="False" DataKeyNames="id_instrumento" OnRowEditing="gvInstrumentos_RowEditing" OnRowDeleting="gvInstrumentos_RowDeleting" OnRowUpdating="gvInstrumentos_RowUpdating" OnRowCancelingEdit="gvInstrumentos_RowCancelingEdit">
+        <Columns>
+            <asp:BoundField DataField="id_instrumento" HeaderText="ID" ReadOnly="True" />
+            <asp:BoundField DataField="tipo_instrumento" HeaderText="Tipo" />
+            <asp:BoundField DataField="marca" HeaderText="Marca" />
+            <asp:BoundField DataField="modelo" HeaderText="Modelo" />
+            <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+        </Columns>
+    </asp:GridView>
+    <asp:Button ID="btnAgregar" runat="server" Text="Agregar Nuevo Instrumento" OnClick="btnAgregar_Click" />
+</asp:Content>
---
+++ Instrumentos.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MelodyHub.instrumentos
+{
+    public partial class Instrumentos : System.Web.UI.Page
+    {
+        String cadenaConexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadInstrumentos();
+            }
+        }
+
+        private void LoadInstrumentos()
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM instrumento", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvInstrumentos.DataSource = dt;
+                gvInstrumentos.DataBind();
+            }
+        }
+
+        protected void gvInstrumentos_RowEditing(object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+        {
+            gvInstrumentos.EditIndex = e.NewEditIndex;
+            LoadInstrumentos();
+        }
+
+        protected void gvInstrumentos_RowCancelingEdit(object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+        {
+            gvInstrumentos.EditIndex = -1;
+            LoadInstrumentos();
+        }
+
+        protected void gvInstrumentos_RowUpdating(object sender, System.Web.UI.WebControls.GridViewUpdateEventArgs e)
+        {
+            int id = Convert.ToInt32(gvInstrumentos.DataKeys[e.RowIndex].Value);
+            string tipo = ((TextBox)gvInstrumentos.Rows[e.RowIndex].Cells[1].Controls[0]).Text;
+            string marca = ((TextBox)gvInstrumentos.Rows[e.RowIndex].Cells[2].Controls[0]).Text;
+            string modelo = ((TextBox)gvInstrumentos.Rows[e.RowIndex].Cells[3].Controls[0]).Text;
+
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE instrumento SET tipo_instrumento=@Tipo, marca=@Marca, modelo=@Modelo WHERE id_instrumento=@Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Tipo", tipo);
+                cmd.Parameters.AddWithValue("@Marca", marca);
+                cmd.Parameters.AddWithValue("@Modelo", modelo);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            gvInstrumentos.EditIndex = -1;
+            LoadInstrumentos();
+        }
+
+        protected void gvInstrumentos_RowDeleting(object sender, System.Web.UI.WebControls.GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(gvInstrumentos.DataKeys[e.RowIndex].Value);
+
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("DELETE FROM instrumento WHERE id_instrumento=@Id", conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadInstrumentos();
+        }
+
+        protected void btnAddNew_Click(object sender, EventArgs e)
+        {
+            // Implementación para agregar un nuevo instrumento.
+        }
+    }
+}
